solved bug that measurements where in the wrong order

dataset.py
@@ -52,6 +52,7 @@ class CarlaDataset:
         path_meas = self.measurements[index]
 
         img = cv2.imread(path, -1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         meas = np.load(path_meas, allow_pickle=True)
         measurements = meas['measurements']
 
@@ -62,11 +63,15 @@ class CarlaDataset:
         img = img.astype("float32") / 255.0
         # crop image to 160x346
         img = img[80:240, :]
-
+        print(measurements)
         speed = np.asarray(measurements[0][0], dtype=float)
+        print("dataset speed: ", speed)
         steer = np.asarray(measurements[5], dtype=float)
+        print("dataset steer: ", steer)
         throttle = np.asarray(measurements[4], dtype=float)
+        print("dataset throttle: ", throttle)
         brake = np.asarray(measurements[6], dtype=float)
+        print("dataset brake: ", brake)
 
         if self.transform:
             img = self.transform(img)

trainer/trainer.py
@@ -71,10 +71,14 @@ class Trainer(BaseTrainer):
         self.train_metrics.reset()
 
         for batch_idx, (data, speed, steer, throttle, brake) in enumerate(self.data_loader):
+            print("trainer speed: ", speed)
+            print("trainer steer: ", steer)
+            print("trainer throttle: ", throttle)
+            print("trainer brake: ", brake)
             data, speed = data.to(self.device), speed.to(self.device)
             # steer, throttle, brake = steer.to(self.device), throttle.to(self.device), brake.to(self.device)
             # target = [steer.to(self.device), throttle.to(self.device), brake.to(self.device), speed.to(self.device)]
-            target = torch.cat((steer.to(self.device), throttle.to(self.device), brake.to(self.device), speed.to(self.device)), dim=1)
+            target = torch.cat((speed.to(self.device), steer.to(self.device), throttle.to(self.device), brake.to(self.device)), dim=1)
             self.optimizer.zero_grad()
             loss_weights = [self.loss_weights['steer'], self.loss_weights['throttle'], 
                             self.loss_weights['brake'], self.loss_weights['speed']]
@@ -93,22 +97,24 @@ class Trainer(BaseTrainer):
             for met in self.metric_ftns:
                 self.train_metrics.update(met.__name__, met(output, target))
 
-            if epoch == 1 and batch_idx == 0:
-                self.writer.add_graph(self.model, [data, speed])
+            # if epoch == 1 and batch_idx == 0:
+                # self.writer.add_graph(self.model, [data, speed])
 
             if batch_idx % self.log_step == 0:
                 self.logger.debug('Train Epoch: {} {} Loss: {:.6f}'.format(
                     epoch,
                     self._progress(batch_idx),
                     loss.item()))
-                self.writer.add_figure('images', plot_classes_preds(data.detach().cpu().numpy()[0:4, :, :, :],
-                                                                    target.detach().cpu().numpy()[0:4, :],
-                                                                    output.detach().cpu().numpy()[0:4, :]))
+                # self.writer.add_figure('images', plot_classes_preds(data.detach().cpu().numpy()[0:4, :, :, :],
+                #                                                    target.detach().cpu().numpy()[0:4, :],
+                #                                                    output.detach().cpu().numpy()[0:4, :]))
                 #self.writer.add_image('input', make_grid(imgs_save.cpu(), nrow=8, normalize=True))
                 self.writer.add_image('images2', make_grid(add_prediction_images(data.detach().cpu().numpy()[0:4, :, :, :],
                                                                     target.detach().cpu().numpy()[0:4, :],
                                                                     output.detach().cpu().numpy()[0:4, :]), nrow=4))
                 self.writer.add_figure('grad_flow', plot_grad_flow(self.model.named_parameters()))
+                self.writer.add_scalar('learning_rate', self.lr_scheduler.get_last_lr()[0])
+
 
 
             if batch_idx == self.len_epoch:
@@ -145,18 +151,24 @@ class Trainer(BaseTrainer):
                 self.valid_metrics.update('loss', loss.item())
                 for met in self.metric_ftns:
                     self.valid_metrics.update(met.__name__, met(output, target))
-                self.writer.add_figure('images', plot_classes_preds(data.detach().cpu().numpy()[0:4, :, :, :],
-                                                                    target.detach().cpu().numpy()[0:4, :],
-                                                                    output.detach().cpu().numpy()[0:4, :]))
-                #self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
-                #self.writer.add_scalar('validation_loss', loss.item())
+        # self.writer.add_figure('images', plot_classes_preds(data.detach().cpu().numpy()[0:4, :, :, :],
+        #                                                    target.detach().cpu().numpy()[0:4, :],
+        #                                                    output.detach().cpu().numpy()[0:4, :]))
+                self.writer.add_image('images2',
+                                      make_grid(add_prediction_images(data.detach().cpu().numpy()[0:4, :, :, :],
+                                                                      target.detach().cpu().numpy()[0:4, :],
+                                                                      output.detach().cpu().numpy()[0:4, :]), nrow=4))
 
-        # add histogram of model parameters to the tensorboard
-        # print("add histogram to tensorboard")
-        for name, p in self.model.named_parameters():
-            self.writer.add_histogram(name, p, bins='auto')
-            self.writer.add_histogram("grad{}".format(name), p.grad.data.cpu().numpy(), bins='auto')
-        # print("Added all histograms")
+        #self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
+        #self.writer.add_scalar('validation_loss', loss.item())
+
+        if epoch % 5 == 0:
+            # add histogram of model parameters to the tensorboard
+            # print("add histogram to tensorboard")
+            for name, p in self.model.named_parameters():
+                self.writer.add_histogram(name, p, bins='auto')
+                self.writer.add_histogram("grad{}".format(name), p.grad.data.cpu().numpy(), bins='auto')
+            # print("Added all histograms")
         return self.valid_metrics.result()
 
     def _progress(self, batch_idx):

utils/util.py
@@ -150,13 +150,9 @@ def add_prediction_images(images, targets, outputs):
     new_images = np.empty([nbr_images*2, images[0].shape[0], images[0].shape[1], images[0].shape[2]])
     height = images[0].shape[1]
     width = images[0].shape[2]
-    print(height)
-    print(width)
-    print(images.shape)
-    print(new_images.shape)
+
     for idx in range(0, nbr_images):
         new_images[idx*2, :, :, :] = images[idx]
-        print(new_images.shape)
         #pred_img = np.zeros_like(images[idx])
         pred_img = Image.new('RGB', (width, height), color=(1,1,1))
         draw = ImageDraw.Draw(pred_img)
@@ -166,8 +162,7 @@ def add_prediction_images(images, targets, outputs):
         text = "Pred: sp={0:.2f}, st={1:.2f}, th={2:.2f}, br={3:.2f}\n True: sp={4:.2f}, st={5:.2f}, th={6:.2f}, br={7:.2f}".format(
             outputs[idx][0], outputs[idx][1], outputs[idx][2], outputs[idx][3],
             targets[idx][0], targets[idx][1], targets[idx][2], targets[idx][3])
-        print(text)
-        print(type(text))
+
         #draw.text((0,0), str(text), (255,255,0), font=font)
         draw.text((0, 0), text, (0, 0, 255))
         draw = ImageDraw.Draw(pred_img)
@@ -175,6 +170,5 @@ def add_prediction_images(images, targets, outputs):
         #pred_img = pred_img.rotate(-90)
         #pred_img = cv2.putText(pred_img, str("Hello World"), (0, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 1), 2)
         new_images[idx*2+1, :, :, :] = np.transpose(np.asarray(pred_img), (2, 0, 1))
-        print(new_images.shape)
 
     return torch.from_numpy(new_images)